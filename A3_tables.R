6942831b3bb092d29b903f8f489751c1c80d9911
devonnenon <devon.nenon@lshtm.ac.uk>
2025-02-13 15:58:24 -0700

    create tables of results for main/interrupted/extended lag analyses

--- A3_tables.R
+++ A3_tables.R
@@ -11,26 +11,42 @@ locationnames <- c("London", "WestMidlands")
 names(locationsplot) <- locationnames
 outcomes <- c("nonext", "cvd", "resp")
 
-table2df <- do.call(rbind, lapply(locationsplot, function(location){
+
+# ----------------
+# Extended lags
+# ----------------
+tabextlag <- table2df <- do.call(rbind, lapply(locationsplot, function(location){
   outcomes_res <- do.call(rbind, lapply(outcomes, function(outcome){
-    row <- mainlist[[location]][[outcome]][["estperc"]]
-    return(row)
-  }))
-  rownames(outcomes_res) <- paste0(outcomes, names(location)) 
-  return(outcomes_res)
-}))
-
-format(signif(table2df, 3), scientific = F)
-
-
-tableextlag <- table2df <- do.call(rbind, lapply(locationsplot, function(location){
-  outcomes_res <- do.call(rbind, lapply(outcomes, function(outcome){
+    cumulest <- signif(extlaglist[[location]][[outcome]][["estperc"]][,1], 3)
+    cumullb <- signif(extlaglist[[location]][[outcome]][["estperc"]][,2], 3)
+    cumulub <- signif(extlaglist[[location]][[outcome]][["estperc"]][,3], 3)
+    cumrow <- paste0(cumulest, " (",cumullb,",",cumulub,")")
     est <- signif(extlaglist[[location]][[outcome]][["tabperc"]][,1], 3)
     lb <- signif(extlaglist[[location]][[outcome]][["tabperc"]][,2], 3)
     ub <- signif(extlaglist[[location]][[outcome]][["tabperc"]][,3], 3)
-    row <- c(est, lb, ub)
+    row <- paste0(est, " (",lb,",",ub,")")
+    row <- c(cumrow, row)
     return(row)
   }))
-  rownames(outcomes_res) <- paste0(outcomes, names(location)) 
+  rownames(outcomes_res) <- paste(outcomes, location) 
   return(outcomes_res)
+})) ; colnames(tabextlag) <- c("net", "lag0", "lag1", "lag2", "lag3", "lag4", "lag5")
+
+noquote(tabextlag)
+
+
+# --------------------
+# Interrupted analysis 
+# --------------------
+locationsint <- c("nkens", "birmcen")
+
+tabint <- do.call(rbind, lapply(locationsint, function(location) {
+  outcomesres <- do.call(rbind, lapply(outcomes, function(outcome){
+    data <- as.data.frame(intlist[[location]][[outcome]][["estperc"]])
+    data$p <- c(NA, intlist[[location]][[outcome]][["p_diff"]][2,5])
+    rownames(data) <- c(paste0(location, outcome, "pre"), 
+                        paste0(location, outcome, "post"))
+    return(data)
+  }))
 }))
+tabint
