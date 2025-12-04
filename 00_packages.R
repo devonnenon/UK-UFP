b7ba6888e3d8ecc0c2ab9561aeca7380c0aeee38
devonnenon <devon.nenon@lshtm.ac.uk>
2025-12-04 13:49:02 +0000

    Clean for publication

--- 00_packages.R
+++ 00_packages.R
@@ -1,9 +1,15 @@
 ################################################################################
-# UFP/mortality project
-
-
-# Packages
-
+# Code for the analysis in:
+# 
+#   Mortality risks associated with short-term exposure to ultrafine particles 
+#   in London and the West Midlands
+# 
+#   Nenon D, Fuller G, Masselot P, Gasparrini, A.
+#   Environmental Epidemiology - 2025
+#
+#
+# Import packages
+#
 ################################################################################
 
 # Data prep/cleaning 
